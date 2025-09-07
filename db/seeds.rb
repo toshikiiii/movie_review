5f9062e66f207fcd84b4a5f3f983dce483dceb83
會田俊紀 <toshiki@aidatoshinorinoMacBook-Air.local>
2025-09-08 05:54:12 +0900

    Seed: admin user only

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
+# seeds.rb
+genres = %w[アクション SF コメディ ホラー サスペンス クライム スリラー ロマンス ミュージカル]
+genre_map = genres.to_h { |name| [name, Genre.find_or_create_by!(name: name)] }
 
-%w[アクション SF コメディ ホラー サスペンス クライム スリラー ロマンス ミュージカル ドラマ].each do |name|
-  Genre.find_or_create_by!(name: name)
+
+User.find_or_create_by!(email: admin_email) do |u|
+  u.name     = "管理者ユーザー"
+  u.password = "password"
+  u.admin    = true
 end
+
+puts "[seed] admin user ensured: #{admin_email}"
